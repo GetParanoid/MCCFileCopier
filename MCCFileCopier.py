f2690e97b1a95037ea7f5f4a27d4c998d9ec6c09
Ryan Hall <ryanhall2537@gmail.com>
2019-12-06 10:57:17 -0500

    Attempted to add support for unlockdb.xml

--- MCCFileCopier.py
+++ MCCFileCopier.py
@@ -17,15 +17,18 @@ print()
 ModFolder = 'MODS'
 ModPak = ModFolder + '/MCC-WindowsNoEditor.pak'
 ModMap = ModFolder + '/forge_halo.map'
+ModUnlock = ModFolder + '/unlockdb.xml'
 
 #Get Vanilla Files Directory(Using steamdir as base directory)
 VanillaFiles = "/Vanilla Files"
 VanillaPak = ModFolder + '/Vanilla Files/MCC-WindowsNoEditor.pak'
 VanillaMap = ModFolder +'/Vanilla Files/forge_halo.map'
+VanillaUnlock = ModFolder + '/Vanilla Files/unlockdb.xml'
 
 #Get MCC file locations for mods and maps
 targetPak = 'MCC/Content/Paks/'
 targetMap = 'haloreach/maps/'
+targetUnlock = 'data/ui'
 
 def copyFiles():
     print("Made by reddit.com/u/GetParanoid - Contact for any issues and/or ideas\n")
@@ -39,8 +42,10 @@ def copyFiles():
                 print("Syncing Vanilla Files to MCC")
                 os.unlink(steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
                 os.unlink(steamdir + targetMap + 'forge_halo.map')
+                os.unlink(steamdir + targetUnlock + 'unlockdb.xml')
                 os.link(steamdir + VanillaPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
                 os.link(steamdir + VanillaMap, steamdir + targetMap + 'forge_halo.map')
+                os.link(steamdir + VanillaUnlock, steamdir + targetUnlock + 'unlockdb.xml')
                 print("COMPLETE")
                 os.system('pause')
             elif userInput == "2":
@@ -48,8 +53,10 @@ def copyFiles():
                 print("Syncing Modded Files to MCC")
                 os.unlink(steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
                 os.unlink(steamdir + targetMap + 'forge_halo.map')
+                os.unlink(steamdir + targetUnlock + 'unlockdb.xml')
                 os.link(steamdir + ModPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
                 os.link(steamdir + ModMap, steamdir + targetMap + 'forge_halo.map')
+                os.link(steamdir + ModUnlock, steamdir + targetUnlock + 'unlockdb.xml')
                 print("COMPLETE")
                 os.system('pause')
             else:
