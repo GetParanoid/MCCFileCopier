00734d39eeb0455ffaa52f25d1abb3198f25d33a
Ryan Hall <ryanhall2537@gmail.com>
2019-12-06 11:47:19 -0500

    reduced code reuse

--- MCCFileCopier.py
+++ MCCFileCopier.py
@@ -30,6 +30,21 @@ targetPak = 'MCC/Content/Paks/'
 targetMap = 'haloreach/maps/'
 targetUnlock = 'data/ui/'
 
+def unlinkFiles():
+    os.unlink(steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
+    os.unlink(steamdir + targetMap + 'forge_halo.map')
+    os.unlink(steamdir + targetUnlock + 'unlockdb.xml')
+
+def linkFiles(modded):
+    if (modded):
+        os.link(steamdir + ModPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
+        os.link(steamdir + ModMap, steamdir + targetMap + 'forge_halo.map')
+        os.link(steamdir + ModUnlock, steamdir + targetUnlock + 'unlockdb.xml')
+    else:
+        os.link(steamdir + VanillaPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
+        os.link(steamdir + VanillaMap, steamdir + targetMap + 'forge_halo.map')
+        os.link(steamdir + VanillaUnlock, steamdir + targetUnlock + 'unlockdb.xml')
+
 def copyFiles():
     print("Made by reddit.com/u/GetParanoid - Contact for any issues and/or ideas\n")
     if os.path.isdir(steamdir + ModFolder) & os.path.isdir(steamdir + ModFolder + VanillaFiles)  == 1:
@@ -40,23 +55,15 @@ def copyFiles():
             if userInput == "1":
                 os.system('cls')
                 print("Syncing Vanilla Files to MCC")
-                os.unlink(steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.unlink(steamdir + targetMap + 'forge_halo.map')
-                os.unlink(steamdir + targetUnlock + 'unlockdb.xml')
-                os.link(steamdir + VanillaPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.link(steamdir + VanillaMap, steamdir + targetMap + 'forge_halo.map')
-                os.link(steamdir + VanillaUnlock, steamdir + targetUnlock + 'unlockdb.xml')
+                unlinkFiles()
+                linkFiles(False)
                 print("COMPLETE")
                 os.system('pause')
             elif userInput == "2":
                 os.system('cls')
                 print("Syncing Modded Files to MCC")
-                os.unlink(steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.unlink(steamdir + targetMap + 'forge_halo.map')
-                os.unlink(steamdir + targetUnlock + 'unlockdb.xml')
-                os.link(steamdir + ModPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.link(steamdir + ModMap, steamdir + targetMap + 'forge_halo.map')
-                os.link(steamdir + ModUnlock, steamdir + targetUnlock + 'unlockdb.xml')
+                unlinkFiles()
+                linkFiles(True)
                 print("COMPLETE")
                 os.system('pause')
             else:
