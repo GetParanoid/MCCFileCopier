abbd4ae24942db6c1b53f281e99f65cf076e7f5d
Grant Sweat <sweatgrant@gmail.com>
2019-12-07 11:05:33 -0600

    Update MCCFileCopier.py

--- MCCFileCopier.py
+++ MCCFileCopier.py
@@ -9,62 +9,96 @@ from findMCC import findMCC
 
 # Check where MCC location is
 steamdir = findMCC()
-print()
-print('MCC Install Location: ' + steamdir)
-print()
+print('\nMCC Install Location: ' + steamdir + "\n")
+
 
 #Get Modded file directory(Using steamdir as base directory)
-ModFolder = 'MODS'
-ModPak = ModFolder + '/MCC-WindowsNoEditor.pak'
-ModMap = ModFolder + '/forge_halo.map'
+ModFolder = '/MODS'
 
 #Get Vanilla Files Directory(Using steamdir as base directory)
-VanillaFiles = "/Vanilla Files"
-VanillaPak = ModFolder + '/Vanilla Files/MCC-WindowsNoEditor.pak'
-VanillaMap = ModFolder +'/Vanilla Files/forge_halo.map'
+VanillaFiles = ModFolder + "/Vanilla Files"
 
-#Get MCC file locations for mods and maps
-targetPak = 'MCC/Content/Paks/'
-targetMap = 'haloreach/maps/'
+
+
+
+# HOW TO ADD A MOD
+#Example Mod Below Copy/Paste to add a new one
+class ModName():                            #create a class with the Mod's name
+    fileName = 'MCC-WindowsNoEditor.pak'    #(Name of the mod's file)
+    target = 'MCC/Content/Paks/'            #(The mod's install location inside MCC)
+        # IMPORTANT : IF the mod has multiple files, add a new class for each file
+
+#create an object for each mod
+class EnableForgePak():
+    fileName = 'MCC-WindowsNoEditor.pak'
+    target = 'MCC/Content/Paks/'
+class ForgeMap():
+    fileName = 'forge_halo.map'
+    target = 'haloreach/maps/'
+class UnearthedDLL():
+    fileName = 'haloreach.dll'
+    target = 'haloreach/'
+class UneartehdMap():
+    fileName = 'cex_prisoner.map'
+    target = 'haloreach/maps/'
+
+#make a list of all mods you want to install / uninstall
+#Add mod to the following list(Do NOT enable conflicting mods ex: same fileName)
+modList = [ForgeMap, EnableForgePak]
+
+def verifyFiles():
+    if not os.path.isdir(steamdir + ModFolder):
+        print('MODS folder not found')
+        return False
+    if not os.path.isdir(steamdir + VanillaFiles):
+        print('Vanilla Files folder not found')
+        return False
+    for mod in modList:
+        if not os.path.isfile(steamdir + ModFolder + '/' + mod.fileName):
+            print(mod.fileName + ' not found in MODS folder')
+            return False
+        if not os.path.isfile(steamdir + VanillaFiles + '/' + mod.fileName):
+            print(mod.fileName + ' not found in Vanilla Files folder')
+            return False
+    print("Files in MODS folder = OK")
+    return True
+
+def unlinkFiles(mod):
+    os.unlink(steamdir + mod.target + mod.fileName)
+
+def linkFiles(mod, modded):
+    if (modded):
+        os.link(steamdir + ModFolder + '/' + mod.fileName, steamdir + mod.target + mod.fileName)
+    else:
+        os.link(steamdir + VanillaFiles + '/' + mod.fileName, steamdir + mod.target + mod.fileName)
 
 def copyFiles():
     print("Made by reddit.com/u/GetParanoid - Contact for any issues and/or ideas\n")
-    if os.path.isdir(steamdir + ModFolder) & os.path.isdir(steamdir + ModFolder + VanillaFiles)  == 1:
-        print("Directories = OK")
-        if os.path.isfile(steamdir + VanillaPak) & os.path.isfile(steamdir + VanillaMap) & os.path.isfile(steamdir + ModPak) & os.path.isfile(steamdir + ModMap) == 1:
-            print("Files in MODS folder = OK\n")
-            userInput = input("ENTER [ 1 ] - Copy Vanilla Files \nENTER [ 2 ] - Copy Modded Files\nINPUT: ")
-            if userInput == "1":
-                os.system('cls')
-                print("Syncing Vanilla Files to MCC")
-                os.unlink(steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.unlink(steamdir + targetMap + 'forge_halo.map')
-                os.link(steamdir + VanillaPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.link(steamdir + VanillaMap, steamdir + targetMap + 'forge_halo.map')
-                print("COMPLETE")
-                os.system('pause')
-            elif userInput == "2":
-                os.system('cls')
-                print("Syncing Modded Files to MCC")
-                os.unlink(steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.unlink(steamdir + targetMap + 'forge_halo.map')
-                os.link(steamdir + ModPak, steamdir + targetPak + 'MCC-WindowsNoEditor.pak')
-                os.link(steamdir + ModMap, steamdir + targetMap + 'forge_halo.map')
-                print("COMPLETE")
-                os.system('pause')
-            else:
-                os.system('cls')
-                print("Invalid input")
-                time.sleep(2)
-                os.system('cls')
-                copyFiles()
-        else:
-            print("Missing Files - Either Vanilla[or]Modded forge_halo.map or MCC-WindowsNoEditor.pak not found within MODS folder")
+    if (verifyFiles()):
+        userInput = input("ENTER [ 1 ] - Copy Vanilla Files \nENTER [ 2 ] - Copy Modded Files\nINPUT: ")
+        if userInput == "1":
+            os.system('cls')
+            print("Syncing Vanilla Files to MCC")
+            for mod in modList:
+                unlinkFiles(mod)
+                linkFiles(mod, False)
+            print("COMPLETE")
             os.system('pause')
+        elif userInput == "2":
+            os.system('cls')
+            print("Syncing Modded Files to MCC")
+            for mod in modList:
+                unlinkFiles(mod)
+                linkFiles(mod, True)
+            print("COMPLETE")
+            os.system('pause')
+        else:
+            os.system('cls')
+            print("Invalid input")
+            time.sleep(2)
+            os.system('cls')
+            copyFiles()
     else:
-        print("Improper File Structure, aborting.")
         os.system('pause')
 
 copyFiles()
-
-
